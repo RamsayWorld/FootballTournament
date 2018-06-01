e1beb6699daa8fd5b14fc8ce9ceeb13f684b6f21
lramsay <lramsay@csc.com>
2018-06-01 15:43:12 +0100

    add first Spock test

--- src/main/java/com/example/football/model/Team.java
+++ src/main/java/com/example/football/model/Team.java
@@ -1,12 +1,20 @@
 package com.example.football.model;
 
+import javax.persistence.Entity;
+
 import lombok.Data;
 
 @Data
+@Entity
 public class Team {
 
     private Long id;
     private String name;
 
+    public Team(Long id, String name) {
+        super();
+        this.id = id;
+        this.name = name;
+    }
 
 }
--- src/test/groovy/com/example/football/model/TeamSpec.groovy
+++ src/test/groovy/com/example/football/model/TeamSpec.groovy
@@ -4,4 +4,27 @@ import spock.lang.Specification
 
 class TeamSpec extends Specification {
 
+
+    def "Two same constructed Team equals each other"(){
+        given:
+            def team = new Team(1,"England")
+            def team2 = new Team(1,"England")
+
+        expect:
+            team == team2
+            team.equals(team2)
+
+    }
+
+    def "Two different constructed Teams don't equal each other"(){
+        given:
+            def team = new Team(1,"England")
+            def team2 = new Team(2,"Scotland")
+
+        expect:
+            team != team2
+            !team.equals(team2)
+
+    }
+
 }
